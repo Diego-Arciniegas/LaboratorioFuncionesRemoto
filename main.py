c4316cf6806b91e6922fba4d702d70acc78b92ea
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2020-04-08 15:23:46 -0500

    Primera modificacion

---
+++ main.py
@@ -0,0 +1,10 @@
+
+valor1 = float(input("ingrese el valor del numero base:"))
+valor2 = float(input("ingrese el valor del numero exponente:"))
+
+import operator
+operator.pow(valor1, valor2)     
+operator.__pow__(valor1, valor2) 
+
+print(operator.pow(valor1, valor2))
+
